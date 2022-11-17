cc800826c210905ce03011b4bf3b14d524e75faf
Andrea055 <vivanco11.ac@gmail.com>
2022-11-17 11:30:51 +0100

    add crypto

---
+++ src/lib/crypto/aes.ts
@@ -0,0 +1,30 @@
+import crypto from 'crypto';
+import { Buffer } from 'node:buffer';
+import dotenv from 'dotenv';
+dotenv.config();
+export function encrypt(text: string) {
+    dotenv.config();
+
+    const pass = crypto.createHash('md5').update(process.env.KEY).digest('hex');
+	const cipher = crypto.createCipheriv(
+		'aes-256-cbc',
+		Buffer.from(pass),
+		Buffer.from(process.env.IV)
+	);
+	let crypted = cipher.update(text, 'utf8', 'hex');
+	crypted += cipher.final('hex');
+	return crypted;
+}
+
+export function decrypt(text: string) {
+    const pass = crypto.createHash('md5').update(process.env.KEY).digest('hex');
+	const decipher = crypto.createDecipheriv(
+		'aes-256-cbc',
+		Buffer.from(pass),
+		Buffer.from(process.env.IV)
+	);
+    if(text == null)    return null;
+	let dec = decipher.update(text, 'hex', 'utf8');
+	dec += decipher.final('utf8');
+	return dec;
+}
